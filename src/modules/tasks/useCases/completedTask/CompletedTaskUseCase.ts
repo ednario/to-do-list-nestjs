a98f80f83663b8092cf79a89ef1a9ae9aac90768
Ednario Andrade <joseednario637@gmail.com>
2023-06-22 11:44:32 -0300

    feat: ensure CompletedTaskUseCase

---
+++ src/modules/tasks/useCases/completedTask/CompletedTaskUseCase.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+
+import { TaskRepository } from '@tasks/repositories/TaskRepository';
+import { TaskNotFound } from '@errors/TaskNotFound';
+
+interface CompletedTaskRequest {
+  taskId: string;
+}
+
+type CompletedTaskResponse = void;
+
+@Injectable()
+export class completedTaskUseCase {
+  constructor(private taskRepository: TaskRepository) {}
+
+  async execute(request: CompletedTaskRequest): Promise<CompletedTaskResponse> {
+    const { taskId } = request;
+
+    const notification = await this.taskRepository.findById(taskId);
+
+    if (!notification) {
+      throw new TaskNotFound();
+    }
+
+    notification.completedTask();
+
+    await this.taskRepository.save(notification);
+  }
+}
