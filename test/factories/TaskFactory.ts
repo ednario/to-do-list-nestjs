81cee032c238498ee16a5170a7a599178798106e
Ednario Andrade <joseednario637@gmail.com>
2023-06-22 11:19:38 -0300

    feat: ensure testing factory for Task

---
+++ test/factories/TaskFactory.ts
@@ -0,0 +1,15 @@
+import { Description } from '@tasks/entities/Description';
+import { Task, TaskProps } from '@tasks/entities/Task';
+import { Title } from '@tasks/entities/Title';
+
+type Override = Partial<TaskProps>;
+
+export function makeTask(override: Override = {}) {
+  const task = new Task({
+    title: new Title('Passeio'),
+    description: new Description('Lever os cães para o passeio'),
+    // dueDate: new Date('December 17, 2023 03:24:00'),
+    ...override,
+  });
+  return task;
+}
