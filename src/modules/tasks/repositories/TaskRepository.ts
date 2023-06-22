825242504837f0dc862358b95d1d8ec9b61e687a
Ednario Andrade <joseednario637@gmail.com>
2023-06-22 11:13:22 -0300

    feat: ensure TaskRepository

---
+++ src/modules/tasks/repositories/TaskRepository.ts
@@ -0,0 +1,8 @@
+import { Task } from '@tasks/entities/Task';
+
+export abstract class TaskRepository {
+  abstract create(task: Task): Promise<void>;
+  abstract listAll(): Promise<Task[]>;
+  abstract findById(taskId: string): Promise<Task | null>;
+  abstract save(task: Task): Promise<void>;
+}
