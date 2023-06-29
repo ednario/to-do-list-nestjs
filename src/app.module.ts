3dc1afd30a78259ab8b4406bc2d3b8ad2e4e0bb8
Ednario Andrade <joseednario637@gmail.com>
2023-06-29 20:30:19 -0300

    feat: ensure the import of HttpModule and DatabaseModule modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 
+import { DatabaseModule } from '@infra/database/database.module';
+import { HttpModule } from '@infra/http/http.module';
+
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [HttpModule, DatabaseModule],
 })
 export class AppModule {}
